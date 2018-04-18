bd296b0a1b243804ac5e3e9df2cccf28c556af40
m-ogawa <ogawamakoto@live.jp>
2018-04-18 22:29:36 +0800

    test2 wav読み込みとグラフ表示
    wav読み込み、FFT、グラフ表示（Plot）

--- test01.py
+++ test01.py
@@ -10,8 +10,8 @@ t = np.linspace(start=0, stop=(2*np.pi)*time*freq, num=time*fs-1, endpoint=False
 sig = np.sin(t)
 sig2 = np.array([sig, sig]).T
 
-sf.write(file='./test01_output.wav', data=sig, samplerate=fs, exclusive_creation=False)
-#sf.write(file='./new_file.wav', data=sig2, samplerate=fs, exclusive_creation=False)
+#sf.write(file='./test01_output.wav', data=sig, samplerate=fs, exclusive_creation=False)
+sf.write(file='./test01_output.wav', data=sig2, samplerate=fs, exclusive_creation=False)
 
-#print(sig)
-#print(sig2.shape[1])
+print(sig2)
+print(sig2.shape[1])
--- test02.py
+++ test02.py
@@ -1,65 +1,36 @@
 import sys
-
 import numpy as np
 import scipy.fftpack as fft
 import matplotlib.pyplot as plt
+import pysoundfile as sf
 
-import soundfile as sf
+plt.close("all")
 
-if __name__ == '__main__':
-    plt.close("all")
+filename = './test01_output.wav'
+#filename = './sample.wav'
+sig, fs = sf.read(filename)
 
-    # wavファイル読み込み
-    filename = sys.argv[1]
-    wav, fs = sf.read(filename)
+sig_l = sig[:, 0]
+sig_r = sig[:, 1]
+n_fft = len(sig_l)
 
-    # ステレオ2chの場合、LchとRchに分割
-    wav_l = wav[:, 0]
-    wav_r = wav[:, 1]
+# FFT
+Zs_l = fft.fft(sig_l, n_fft)
+Zs_l = fft.fft(sig_l, n_fft)
 
-    # 入力をモノラル化
-    xs = (0.5 * wav_l) + (0.5 * wav_r)
 
-    n_len = len(xs)
-    n_fft = 128
-    n_overlap = 2
-    n_shift = n_fft / n_overlap
+# Plot
+fig = plt.figure(1, figsize=(8, 8))
+ax = fig.add_subplot(211)
+ax.plot(sig_l[:1000])
+ax.set_title("input signal")
+ax.set_xlabel("time [pt]")
+ax.set_ylabel("amplitude")
 
-    # 中間バッファ
-    zs = np.zeros(n_len)
-    Zs = np.zeros(n_fft)
+ax = fig.add_subplot(212)
+ax.plot(Zs_l[0:fs/2])
+ax.set_title("FFT result")
+ax.set_xlabel("Freq [Hz]")
+ax.set_ylabel("amplitude")
 
-    # 出力バッファ
-    ys = np.zeros(n_len)
-
-    # 窓関数
-    window = np.hanning(n_fft)
-
-    # FFT & IFFT
-    for start in range(0, n_len - n_shift, n_shift):
-        xs_cut = xs[start: start + n_fft]
-        xs_win = xs_cut * window
-        Xs = fft.fft(xs_win, n_fft)
-
-        # some signal processing
-        Zs = Xs
-        zs = fft.ifft(Zs, n_fft)
-
-        # write output buffer
-        ys[start: start + n_fft] += np.real(zs)
-
-    # 冒頭から10秒分プロット
-    fig = plt.figure(1, figsize=(8, 10))
-    ax = fig.add_subplot(211)
-    ax.plot(xs[:fs*10])
-    ax.set_title("input signal")
-    ax.set_xlabel("time [pt]")
-    ax.set_ylabel("amplitude")
-
-    ax = fig.add_subplot(212)
-    ax.plot(ys[:fs*10])
-    ax.set_title("output signal")
-    ax.set_xlabel("time [pt]")
-    ax.set_ylabel("amplitude")
-
-    plt.show()
+plt.show()
